071850a8bfd612c99d3a6a68c2089d8eaedc79de
neviko <nevosayag@gmail.com>
2016-05-05 12:37:45 +0300

    New changelist

--- DiffSign2/app/learningMode/Letters/Learning_LettersController.js
+++ DiffSign2/app/learningMode/Letters/Learning_LettersController.js
@@ -1,11 +1,24 @@
 (function(app){
 
-    function Learning_LettersController($scope) {
-        $scope.message='הלימוד של אותיות';
-    }
+    function Learning_LettersController($scope,$http,$routeParams) {
+        $scope.message='מאגר האותיות';
+
+        var onUsersComplete = function(response) {
+            $scope.users = response.data;
+        };
 
 
-    app.controller('Learning_LettersController',Learning_LettersController); 
+        var onRepos = function(data) {
+            $scope.repos = data;
+        };
+
+        $http.get("https://api.github.com/users").then(onUsersComplete);
+
+
+    };
+
+
+    app.controller('Learning_LettersController',Learning_LettersController);
 
 
 })( angular.module('diffSign'));

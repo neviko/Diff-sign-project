40d71fb8ddaefc06d268ae846c6d64848411de9f
uria <uriasho@gmail.com>
2016-05-15 15:55:21 +0300

    deleted DiffSign2 folder in order to rearrnge the app folders

--- DiffSign2/db/db.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://diff:diff123@ds042138.mlab.com:42138/DiffSign_MongoDB');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});

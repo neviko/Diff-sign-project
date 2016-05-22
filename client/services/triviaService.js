e4ee595d94c6a75d18168de9426d385d19255998
neviko <nevosayag@gmail.com>
2016-05-22 10:39:52 +0300

    NEVO AND RUSSO COMMIT
    try to add timer and put the game in loop, change design

--- client/services/triviaService.js
+++ client/services/triviaService.js
@@ -5,6 +5,7 @@
 (function(){
 
     var globalCurrClip;
+    var numOfQuestions=0;
     angular.module('diffSign').factory('triviaService', Service);
 
     function Service($http) {
@@ -20,7 +21,7 @@
         service.setAnswers = setAnswers;
         service.mixArrayObjects = mixArrayObjects;
         service.onUserChooseAnswer = onUserChooseAnswer;
-        service.timerSet = timerSet;
+        service.initQuststions = initQuststions;
         //service.upLeftPicClick = upLeftPicClick;
         //service.upRightPicClick = upRightPicClick;
         //service.downLeftPicClick = downLeftPicClick;
@@ -34,15 +35,20 @@
         //method to choose array element that not choose yet
         function currObj(clips) {
 
-            while (1) {
+            while ( numOfQuestions < clips.length) {
                 var randClip = Math.floor((Math.random() * clips.length) );
                 if (clips[randClip].isViewed == false)
                 {
                     globalCurrClip=clips[randClip];
                     clips[randClip].isViewed = true;
-                   return clips[randClip];
+                    numOfQuestions++;
+                    return clips[randClip];
                 }
+
             }
+
+            console.log("game over");
+            return "game over";
         }
 
 
@@ -177,9 +183,76 @@
 
         }
         
-        function timerSet()
+        function initQuststions()
         {
-            //make the timer set. the timer will restart when a clip change.
+            var clips = [{
+                "_id": {
+                    "$oid": "572798cfc80eb653c3020a75"
+                },
+                "name_en": "father",
+                "name_heb": "אבא",
+                "category": "general",
+                "clip_url": "https://diffsign.blob.core.windows.net/clips/general/father.MOV",
+                "pic_url": "games/trivia/imagesTemp/1.jpg",
+                "isViewed": false
+            },
+                {
+                    "_id":   {
+                        "$oid": "572798fec80eb653c3020a79"
+                    },
+                    "name_en": "ground",
+                    "name_heb": "אדמה",
+                    "category": "general",
+                    "clip_url": "https://diffsign.blob.core.windows.net/clips/general/ground.MOV",
+                    "pic_url": "games/trivia/imagesTemp/2.jpg",
+                    "isViewed": false
+
+                },
+                {
+                    "_id": {
+                        "$oid": "5728954efca0fd6392731d11"
+                    },
+                    "name_en": "dinner",
+                    "name_heb": "ארוחת_ערב",
+                    "category": "house",
+                    "clip_url": "https://diffsign.blob.core.windows.net/clips/general/dinner.MOV",
+                    "pic_url": "games/trivia/imagesTemp/3.jpg",
+                    "isViewed": false
+                },
+                {
+                    "_id": {
+                        "$oid": "572897b2fca0fd6392731e50"
+                    },
+                    "name_en": "how_do_you_feel",
+                    "name_heb": "?איך_אתה_מרגיש",
+                    "category": "general",
+                    "clip_url": "https://diffsign.blob.core.windows.net/clips/general/how_do_you_feel.MOV",
+                    "pic_url": "games/trivia/imagesTemp/3.jpg",
+                    "isViewed": false
+                },
+                {
+                    "_id": {
+                        "$oid": "57289828fca0fd6392731e65"
+                    },
+                    "name_en": "i_love_you",
+                    "name_heb": "אני_אוהב_אותך",
+                    "category": "general",
+                    "clip_url": "https://diffsign.blob.core.windows.net/clips/general/i_love_you.MOV",
+                    "pic_url": "games/trivia/imagesTemp/4.png",
+                    "isViewed": false
+                },
+                {
+                    "_id": {
+                        "$oid": "572898ccfca0fd6392731e71"
+                    },
+                    "name_en": "lunch",
+                    "name_heb": "ארוחת_צהריים",
+                    "category": "general",
+                    "clip_url": "https://diffsign.blob.core.windows.net/clips/general/lunch.MOV",
+                    "pic_url": "games/trivia/imagesTemp/4.png",
+                    "isViewed": false
+                }];
+            return clips;
         }
         
     }
--- client/views/dashboard/games/trivia/triviaController.js
+++ client/views/dashboard/games/trivia/triviaController.js
@@ -1,139 +1,86 @@
 (function (app) {
+    var timeOut;
 
-    var clips = [{
-        "_id": {
-            "$oid": "572798cfc80eb653c3020a75"
-        },
-        "name_en": "father",
-        "name_heb": "אבא",
-        "category": "general",
-        "clip_url": "https://diffsign.blob.core.windows.net/clips/general/father.MOV",
-        "pic_url": "games/trivia/imagesTemp/1.jpg",
-        "isViewed": false
-    },
-        {
-            "_id":   {
-                "$oid": "572798fec80eb653c3020a79"
-            },
-            "name_en": "ground",
-            "name_heb": "אדמה",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/ground.MOV",
-            "pic_url": "games/trivia/imagesTemp/2.jpg",
-            "isViewed": false
+    var triviaController = function ($scope, triviaService,videoService , $interval,$timeout) {
 
-        },
-        {
-            "_id": {
-                "$oid": "5728954efca0fd6392731d11"
-            },
-            "name_en": "dinner",
-            "name_heb": "ארוחת_ערב",
-            "category": "house",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/dinner.MOV",
-            "pic_url": "games/trivia/imagesTemp/3.jpg",
-            "isViewed": false
-        },
-        {
-            "_id": {
-                "$oid": "572897b2fca0fd6392731e50"
-            },
-            "name_en": "how_do_you_feel",
-            "name_heb": "?איך_אתה_מרגיש",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/how_do_you_feel.MOV",
-            "pic_url": "games/trivia/imagesTemp/3.jpg",
-            "isViewed": false
-        },
-        {
-            "_id": {
-                "$oid": "57289828fca0fd6392731e65"
-            },
-            "name_en": "i_love_you",
-            "name_heb": "אני_אוהב_אותך",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/i_love_you.MOV",
-            "pic_url": "games/trivia/imagesTemp/4.png",
-            "isViewed": false
-        },
-        {
-            "_id": {
-                "$oid": "572898ccfca0fd6392731e71"
-            },
-            "name_en": "lunch",
-            "name_heb": "ארוחת_צהריים",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/lunch.MOV",
-            "pic_url": "games/trivia/imagesTemp/4.png",
-            "isViewed": false
-        }];
-
-    var triviaController = function ($scope, triviaService,videoService , $interval) {
-
-        
-
-       var startCountDown = function() {
-        console.log("////////////IN startCountDown//////////////// ");
-        $interval(decrementTimer,1000,20);
-       };
-
-         var decrementTimer = function() {
-        console.log("////////////IN decrementTimer//////////////// ")
-        $scope.timer-=1;
-        if($scope.timer==0){
-            $scope.messageAfterAnswer= "TIME OUT MOTHER FUCKER !"
-        }
-    };
-           
         $scope.score = 0;
-        $scope.timer = 20;
+        $scope.timer = 10;
         $scope.lives = 5;
+        $scope.isRunning=true;
+        $scope.isGameOver = false;
         $scope.messageAfterAnswer="";
-        startCountDown();
 
 
 
-        $scope.currClip = triviaService.currObj(clips);//now trivia service have this mathood because we externalize it in triviaService page.
-        var answers=[{triviaObj:"",isCorrect:false}];
-        triviaService.setAnswers(clips,answers);
-        console.log(answers);
-        // here answers contain 4 different answers
-        triviaService.mixArrayObjects(answers);
-        console.log(answers);
-        $scope.ansArr=answers;
+        var clips = triviaService.initQuststions();
 
 
-     
-        
-        
+
+        var answers=[];
+
+        function loadQuestion(){
+
+
+            $scope.currClip = triviaService.currObj(clips);//now trivia service have this mathood because we externalize it in triviaService page.
+            if($scope.currClip =="game over")
+            {
+                messageAfterAnswer="game over";
+                clearTimeout(timeOut);
+                $scope.isRunning=false;
+                $scope.isGameOver=true;
+                return;
+
+            }
+
+            answers=[{triviaObj:"",isCorrect:false}];
+            triviaService.setAnswers(clips,answers);
+            console.log(answers);
+            //here answers contain 4 different answers
+            triviaService.mixArrayObjects(answers);
+            console.log(answers);
+            $scope.ansArr=answers;
+            $scope.messageAfterAnswer="";
+        }
+
+    if($scope.currClip !="game over")
+        loadQuestion();
+
         //this function bind the dom with the trivia service because on ng-click we must call to function in $scope controller!
         $scope.onUserWordClick = function (e) {
 
-
             var elementId = $(e.target).data('id'); // get the element id from dom
 
-           var isCorrect= triviaService.onUserChooseAnswer(answers,elementId);
+            var isCorrect= triviaService.onUserChooseAnswer(answers,elementId);
             if(isCorrect){
-               $scope.score+=1; $scope.messageAfterAnswer="תשובה נכונה !";
-
+                $scope.score+=1;
+                $scope.messageAfterAnswer="תשובה נכונה !";
             }
 
 
             else{
                 $scope.messageAfterAnswer="תשובה שגויה יה זלמה !...חבל";
-                
+
                 if($scope.lives==0)
                 {
-                   $scope.messageAfterAnswer = "המשחק נגמר ! הניקוד שצברת הינו : " + $scope.score;  
+                    $scope.messageAfterAnswer = "המשחק נגמר ! הניקוד שצברת הינו : " + $scope.score;
                 }
                 else
                 {
                     $scope.lives--;
-        
+
                 }
-                
+
             }
 
+
+            timeOut =  setTimeout(function(){
+                console.log("in delay");
+            }, 4000);
+
+
+
+            $scope.messageAfterAnswer="";
+            loadQuestion();
         };
 
 
@@ -141,15 +88,19 @@
         {
             return videoService.trustSrc(src);
         };
-        
-        
-       
+
+
+
+       function aabb(){
+            state.reload();
+        }
+
 
 
     };
 
 
-    app.controller('triviaController', ["$scope", "triviaService","videoService" , "$interval",triviaController]); // declare on the controller and run loginController
+    app.controller('triviaController', ["$scope", "triviaService","videoService" , "$interval","$timeout",triviaController]); // declare on the controller and run loginController
 
 
 })(angular.module('diffSign'));

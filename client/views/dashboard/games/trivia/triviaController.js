e1cf746f505587330847b71899c9178b9c9decd8
neviko <nevosayag@gmail.com>
2016-05-28 02:21:07 +0300

    NEVO AND RUSSO COMMIT
    trivia game
    
    1- add timer for each question
    2-add button next question
    3-change visual game
    4- change style

--- client/views/dashboard/games/trivia/triviaController.js
+++ client/views/dashboard/games/trivia/triviaController.js
@@ -5,11 +5,14 @@
 
         $scope.score = 0;
         $scope.timer = 10;
-        $scope.lives = 5;
+        $scope.lives = 3;
         $scope.isRunning=true;
+        $scope.showAnswers=false;
         $scope.isGameOver = false;
+        $scope.showNextQuesBtn=false;
+        $scope.customStyle = {}; // styled user message
         $scope.messageAfterAnswer="";
-
+        $scope.returnInterval;
 
 
         var clips = triviaService.initQuststions();
@@ -19,13 +22,21 @@
         var answers=[];
 
         function loadQuestion(){
+            $scope.messageAfterAnswer="";
+            $scope.showAnswers=true;
+            $scope.showNextQuesBtn=false;
+            $scope.timer=10;
+            $scope.showAnswers=false;
+            $scope.onUserButtonClick = function (e){
+                loadQuestion();
 
+            };
 
             $scope.currClip = triviaService.currObj(clips);//now trivia service have this mathood because we externalize it in triviaService page.
             if($scope.currClip =="game over")
             {
-                messageAfterAnswer="game over";
-                clearTimeout(timeOut);
+                $scope.messageAfterAnswer="game over";
+                //clearTimeout(timeOut);
                 $scope.isRunning=false;
                 $scope.isGameOver=true;
                 return;
@@ -39,7 +50,8 @@
             triviaService.mixArrayObjects(answers);
             console.log(answers);
             $scope.ansArr=answers;
-            $scope.messageAfterAnswer="";
+
+            $scope.onUserWordClick;
         }
 
     if($scope.currClip !="game over")
@@ -48,39 +60,35 @@
         //this function bind the dom with the trivia service because on ng-click we must call to function in $scope controller!
         $scope.onUserWordClick = function (e) {
 
+            $interval.cancel(returnInterval);
+            $scope.showAnswers=false;
+            $scope.showNextQuesBtn=true;
             var elementId = $(e.target).data('id'); // get the element id from dom
 
             var isCorrect= triviaService.onUserChooseAnswer(answers,elementId);
             if(isCorrect){
-                $scope.score+=1;
+                $scope.turnGreen();
+                $scope.score+=10*($scope.timer);
                 $scope.messageAfterAnswer="תשובה נכונה !";
             }
 
 
             else{
+                $scope.lives--;
+                $scope.turnRed();
                 $scope.messageAfterAnswer="תשובה שגויה יה זלמה !...חבל";
 
                 if($scope.lives==0)
                 {
                     $scope.messageAfterAnswer = "המשחק נגמר ! הניקוד שצברת הינו : " + $scope.score;
-                }
-                else
-                {
-                    $scope.lives--;
-
+                    $scope.isRunning=false;
                 }
 
             }
 
 
-            timeOut =  setTimeout(function(){
-                console.log("in delay");
-            }, 4000);
-
-
-
-            $scope.messageAfterAnswer="";
-            loadQuestion();
+            //$scope.messageAfterAnswer="";
+            $scope.onUserButtonClick;
         };
 
 
@@ -91,14 +99,50 @@
 
 
 
-       $scope.aabb = function(){
+       $scope.reGame = function(){
            console.log("reloadPage");
             location.reload();
         }
 
 
+        var vid = document.getElementById("videoId");
+        vid.onpause = function() {
+
+            $scope.showAnswers=true;
+             returnInterval = $interval(function(){
+                $scope.timer--;
+
+                if($scope.timer==0)
+                {
+                    $scope.lives--;
+                    $interval.cancel(returnInterval); // stop the timer
+                    $scope.messageAfterAnswer="לא הספקת לענות..חבל";
+                    $scope.showAnswers=false;
+                    $scope.showNextQuesBtn=true;
+                }
+            }, 1000)
+        };
+
+
+
+
+
+        $scope.turnGreen = function (){
+            //what to do here?
+            $scope.messageAfterAnswer.fontcolor("green");
+        };
+
+        $scope.turnRed = function() {
+            $scope.messageAfterAnswer.colorClass="red";
+        };
+
 
     };
+
+
+
+
+
 
 
     app.controller('triviaController', ["$scope", "triviaService","videoService" , "$interval","$timeout",triviaController]); // declare on the controller and run loginController

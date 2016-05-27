f955ad85fc8f66b5d52d420398f1566c39600c37
raphym <elieraphm@gmail.com>
2016-05-27 18:16:00 +0300

    style of login application

--- client/views/loginController.js
+++ client/views/loginController.js
@@ -5,15 +5,35 @@
 app.controller("LoginCtrl",["$scope","$location",
     function(r,t)
     {
-        r.submit=function()
+
+        
+        r.connect = function()
         {
             return t.path("/dashboard"),!1
-        }
+        };
         
         
+        r.userName = document.getElementById("userName").value;
+        r.theEmail = document.getElementById("theEmail").value;
+        r.password = document.getElementById("password").value;
+        r.validatePassword = document.getElementById("validatePassword").value; 
+        r.messagePassword="";
+
+        r.myVar = false;
         
+        r.toggle = function() 
+        {
+            r.myVar = !r.myVar;
+        };
         
-        
+        r.register = function()
+        {
+            if(r.password != r.validatePassword)
+                r.messagePassword = "הסיסמה שלך לא תקין";
+            
+            else 
+                r.messagePassword = "";           
+        };
         
     }
                             

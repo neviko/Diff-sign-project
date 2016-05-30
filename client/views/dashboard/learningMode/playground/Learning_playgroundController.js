2bd3d0f40b7ba30a1e3c7486c983bf7bab2327e8
uria <uriasho@gmail.com>
2016-05-30 12:21:23 +0300

    Update Learning_playgroundController to get clips from db

--- client/views/dashboard/learningMode/playground/Learning_playgroundController.js
+++ client/views/dashboard/learningMode/playground/Learning_playgroundController.js
@@ -1,74 +1,18 @@
-function Learning_playgroundController($scope,$http,videoService) 
+function Learning_playgroundController($scope,$http,$interval,videoService,dbService) 
 {
     $scope.message='גן ילדים';
-    $scope.clips=  [
-        {
-            "_id": 
-            {
-                "$oid": "572798cfc80eb653c3020a75"
-            },
-            "name_en": "father",
-            "name_heb": "אבא",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/father.MOV",
-            "pic_url": ""
-            },
-        {
-            "_id":
-            {
-                "$oid": "572798fec80eb653c3020a79"
-            },
-            "name_en": "soil",
-            "name_heb": "אדמה",
-            "category": "playground",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/playground/soil.MOV",
-            "pic_url": ""
-        },
-        {
-            "_id": 
-            {
-                "$oid": "5728954efca0fd6392731d11"
-            },
-            "name_en": "dinner",
-            "name_heb": "ארוחת_ערב",
-            "category": "house",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/house/dinner.MOV",
-            "pic_url": ""
-        },
-        {
-            "_id": 
-            {
-                "$oid": "572897b2fca0fd6392731e50"
-            },
-            "name_en": "how_do_you_feel",
-            "name_heb": "?איך_אתה_מרגיש",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/how_do_you_feel.MOV",
-            "pic_url": ""
-        },
-        {
-            "_id": 
-            {
-                "$oid": "57289828fca0fd6392731e65"
-            },
-            "name_en": "i_love_you",
-            "name_heb": "אני_אוהב_אותך",
-            "category": "general",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/general/i_love_you.MOV",
-            "pic_url": ""
-        },
-        {
-            "_id":
-            {
-                "$oid": "572898ccfca0fd6392731e71"
-            },
-            "name_en": "lunch",
-            "name_heb": "ארוחת_צהריים",
-            "category": "house",
-            "clip_url": "https://diffsign.blob.core.windows.net/clips/house/lunch.MOV",
-            "pic_url": ""
+    $scope.clips =  [];
+    
+    //----------- Get the db table
+    var category = 'playground';
+    var table_list = dbService.get_table(category);
+    var wait_db = $interval(function() {
+        // When server returned the table
+        if (table_list.$$state.status > 0) {
+            $scope.clips = table_list.$$state.value.data;
+            $interval.cancel(wait_db);
         }
-    ];
+    }, 50);
        
     // return a declaration that the url is trust
     $scope.goTrust = function(src)

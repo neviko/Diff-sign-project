7b101a1455c3b0c59658fdae185313f884278df0
Itai Russo <itai.russo@yahoo.com>
2016-06-04 19:41:07 +0300

    RUSSO AND NEVO maill to shai-project maill if there is any problem or feedback

--- client/views/dashboard/contactUs/contactUsController.js
+++ client/views/dashboard/contactUs/contactUsController.js
@@ -1,11 +1,15 @@
 (function(app) {
 
     var contactUsController= function($scope){
-
+         $scope.isClicked=false;
+        $scope.body="";
         $scope.name = "";
         $scope.email="";
 
         $scope.clicked = function () {
+            
+            window.location.href = "mailto:itairusso0@gmail.com?subject=Shai-Project&body="+$scope.body;
+            $scope.isClicked=true;
             if($scope.name == "")
             {
                 $scope.message = " תודה רבה ";
@@ -16,6 +20,9 @@
 
                 $scope.message = " תודה רבה " + $scope.name;
             }
+            
+                        
+            
         };
 
     };
